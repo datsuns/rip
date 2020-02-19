a9565ee433113dec229f74c570163c0b5f73c65f
datsuns <the.skeleton7@gmail.com>
2020-02-19 14:46:05 +0900

    initial

---
+++ rip/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rip
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
