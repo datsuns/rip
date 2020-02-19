e505d04cdb047e47855529a93df898334e1b4bd0
datsuns <the.skeleton7@gmail.com>
2020-02-19 14:51:34 +0900

    change project type to service

--- rip/Program.cs
+++ rip/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace rip
 {
-    class Program
+    static class Program
     {
-        static void Main(string[] args)
+        /// <summary>
+        /// アプリケーションのメイン エントリ ポイントです。
+        /// </summary>
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            ServiceBase[] ServicesToRun;
+            ServicesToRun = new ServiceBase[]
+            {
+                new Service1()
+            };
+            ServiceBase.Run(ServicesToRun);
         }
     }
 }
